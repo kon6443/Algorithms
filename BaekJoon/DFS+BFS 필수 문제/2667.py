f2ef3aa38dd27bd5a157bc9f907190c728a3b217
kon6443@gmail.com <kon6443@gmail.com>
2022-03-24 16:05:11 +0900

    preparing coding test

--- BaekJoon/DFS+BFS 필수 문제/2667.py
+++ BaekJoon/DFS+BFS 필수 문제/2667.py
@@ -1,17 +1,33 @@
 
-def main():
-    n = int(input())
-    square = []
-    for _ in range(n):
-        square.append(list(map(int, input())))
+def dfs(x, y):
+    global visited, numOfApt
+    numOfApt = numOfApt + 1
+    visited[x][y] = 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0<=nx<len(square) and 0<=ny<len(square):
+            if visited[nx][ny] == 0 and square[nx][ny] == 1:
+                dfs(nx,ny)
 
-    print2DList(square)
+n = int(input())
+visited = [[0]*(n) for _ in range(n)]
+square = []
+for _ in range(n):
+    square.append(list(map(int, input())))
+# up, down, left, right
+dy = [1, -1, 0, 0]
+dx = [0, 0, -1, 1]
+numOfApt = 0
+aptList = []
+for i in range(n):
+    for j in range(n):
+        if square[i][j] == 1 and visited[i][j] == 0:
+            dfs(i, j)
+            aptList.append(numOfApt)
+            numOfApt = 0
+aptList.sort()
 
-def print2DList(l):
-    for i in range(len(l)):
-        for j in range(len(l[0])):
-            print(l[i][j], end=' ')
-        print()
-
-if __name__ == '__main__':
-    main()
+print(len(aptList))
+for i in range(len(aptList)):
+    print(aptList[i])

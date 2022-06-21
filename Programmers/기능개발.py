b97827c23bdd0b5373e8c567b8eaf4f56cbc2192
Onam Kwon <kon6443@gmail.com>
2022-06-21 23:16:45 +0900

    algorithms

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -5,10 +5,10 @@ def solution(progresses, speeds):
     requiredDays = []
     for i in range(len(progresses)):
         requiredDays.append(math.ceil((100-progresses[i])/speeds[i]))
-    while True:
-        if len(requiredDays)<=0: break
+    while len(requiredDays)>0:
         # Subtracting ith value from every element in requiredDays list
         requiredDays = [x - abs(requiredDays[0]) for x in requiredDays]
+        
         numOfPublish = 0
         for i in range(len(requiredDays)):
             if requiredDays[i]<=0:

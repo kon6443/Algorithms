5ed5f6de163bd78ac34af8971e16e4c145dc1d1d
Onam Kwon <kon6443@gmail.com>
2022-07-02 16:46:50 +0900

    algorithms

--- Sorting/bubbleSort.py
+++ Sorting/bubbleSort.py
@@ -1,7 +1,7 @@
 def bubble_sort(list):
     for i in range(len(list)):
         for j in range(len(list)-1-i):
-            if our_list[j] > our_list[j+1]:
+            if list[j] > list[j+1]:
                 # Swap
-                our_list[j], our_list[j+1] = our_list[j+1], our_list[j]
+                our_list[j], list[j+1] = list[j+1], list[j]
 

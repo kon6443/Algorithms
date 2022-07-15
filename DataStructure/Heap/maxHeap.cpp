fe4cc9aa3adbca92211bbad6fa63d1db542a959d
Onam Kwon <kon6443@gmail.com>
2022-07-15 16:00:20 +0900

    maxHeap cpp

--- DataStructure/Heap/maxHeap.cpp
+++ DataStructure/Heap/maxHeap.cpp
@@ -7,6 +7,7 @@
 // time
 #include <time.h>
 
+// 2^8
 #define HEAP_SIZE 256
 #define ARRAY_SIZE 10
 
@@ -33,10 +34,8 @@ int size() {
 
 void push(int data) {
 
-	// 힙의 가장 끝에 데이터 추가
 	heap[++heap_count] = data;
 
-	// 아래의 과정은 child를 parent와 비교하면서 알맞은 위치로 하나씩 올리는 부분
 	int child = heap_count;
 	int parent = child / 2;
 	while (child > 1 && heap[parent] < heap[child]) {
@@ -49,13 +48,13 @@ void push(int data) {
 
 int pop() {
 
-    // returning the first element of a heap DS.
+        // returning the first element of a heap DS.
 	int result = heap[1];
 
 	swap(&heap[1], &heap[heap_count]);
 	heap_count--;
 
-	// 아래의 과정은 child를 parent와 비교하면서 알맞은 위치로 하나씩 내리는 부분
+	// Reconstructing tree structure after popping a root node.
 	int parent = 1;
 	int child = parent * 2;
 	if (child + 1 <= heap_count) {
@@ -80,10 +79,10 @@ int main() {
 	
 	srand(time(NULL));
 
-    // Generating random number from 1 to 50.
+        // Generating random number from 1 to 50.
 	for (int i = 0; i < ARRAY_SIZE; i++) arr[i] = rand() % 50 + 1;
 
-    printf("Pushing into a max heap: \n");
+        printf("Pushing random numbers repeatedly into a max heap: \n");
 	for (int i=0; i<ARRAY_SIZE; i++) push(arr[i]);
 	for (int i=0; i<ARRAY_SIZE; i++) printf("%d ", arr[i]);
 	printf("\n\n");

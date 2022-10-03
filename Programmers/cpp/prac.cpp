d23f04801ad946e6edcdc4769c9c81f8387330be
Onam Kwon <kon6443@gmail.com>
2022-10-03 21:45:00 +0900

    prac

--- Programmers/cpp/prac.cpp
+++ Programmers/cpp/prac.cpp
@@ -12,7 +12,7 @@ void pracMap();
 void pracConvert();
 template<typename T>
 void printVector(vector<T> &v);
-string getContinents(vector<string> &, int &, int &);
+string getContinents(vector<string> &, int , int );
 bool isCursorOut(int &, int &);
 
 int n, m;
@@ -22,27 +22,36 @@ int dy[4] = {0, 1, 0, -1};
 
 int main() {
     vector<string> worldMap;
-    worldMap.push_back("AAB..");
-    worldMap.push_back("ABB.C");
-    worldMap.push_back("AAB.C");
-    worldMap.push_back(".CC.C");
-    worldMap.push_back("C....");
+    worldMap.push_back("AAB...DA");
+    worldMap.push_back("ABB.C.AD");
+    worldMap.push_back("AAB.C.DA");
+    worldMap.push_back(".CC.C.AD");
+    worldMap.push_back("C.....CC");
+    worldMap.push_back("..DDD...");
     n = worldMap.size();
     m = worldMap[0].size();
+    cout<<"n, m: "<<n<<", "<<m<<endl;
     for(const auto &item: worldMap) cout<<item<<endl;
     attendance = vector<vector<bool>> (n, vector<bool>(m, false));
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            if(worldMap[i][j]=='.') attendance[i][j] = true;
+        }
+    }
     vector<string> continents;
-    for(int i=0;i<attendance.size();i++) {
-        for(int j=0;j<attendance[0].size();j++) {
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
             if(attendance[i][j]) continue;
             else {
-                continents.push_back(getContinents(worldMap, i, j));
+                string continent = getContinents(worldMap, i, j);
+                sort(continent.begin(), continent.end());
+                continents.push_back(continent);
             }
         }
     }
-    sort(continents.begin(), continents.end());
     for(int i=0;i<continents.size();i++) {
-        for(int j=0;j<continents[0].size();j++) {
+        cout<<i+1<<"th continent's size: "<<continents[i].size()<<", ";
+        for(int j=0;j<continents[i].size();j++) {
             cout<<continents[i][j]<<" ";
         }cout<<endl;
     }
@@ -50,17 +59,17 @@ int main() {
 }
 
 bool isCursorOut(int &x, int &y) {
-    return x<0 || x>=n || y<0 || y>=n;
+    return x<0 || x>=n || y<0 || y>=m;
 }
 
-string getContinents(vector<string> &worldMap, int &x, int &y) {
+string getContinents(vector<string> &worldMap, int x, int y) {
     string continent = "";
-    if(isCursorOut(x, y) || attendance[x][y]) return NULL;
+    if(isCursorOut(x, y) || attendance[x][y]) return "";
     continent += worldMap[x][y];
-    attendance[x][y] == true;
+    attendance[x][y] = true;
     for(int i=0;i<4;i++) {
         int nx = x+dx[i];
-        int ny = x+dy[i];
+        int ny = y+dy[i];
         continent += getContinents(worldMap, nx, ny);
     }
     return continent;

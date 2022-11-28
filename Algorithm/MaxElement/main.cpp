9fe79022737706fca65c563a364d788929644fff
Onam Kwon <kon6443@gmail.com>
2022-11-28 09:48:56 +0900

    max_element and min_element

--- Algorithm/MaxElement/main.cpp
+++ Algorithm/MaxElement/main.cpp
@@ -10,6 +10,7 @@ int main() {
     cout<<"v: ";
     for(const auto &item: v) cout<<item<<" "; cout<<endl;
 
+    // max_element()
     // Returning max element's iterator.
     // You have to use pointer to access the object. => *t
     auto t = max_element(v.begin(), v.end());
@@ -18,6 +19,17 @@ int main() {
     // To get the index of the value, get the distance from the beginning to itself. 
     int index = distance(v.begin(), t); 
     cout<<"index of the maximum value: "<<index<<endl;
+
+
+    // min_element()
+    // Returning min element's iterator.
+    // You have to use pointer to access the object. => *t
+    auto c = min_element(v.begin(), v.end());
+    cout<<"*c: "<<*c<<endl;
+
+    // To get the index of the value, get the distance from the beginning to itself. 
+    int indexC = distance(v.begin(), c);
+    cout<<"index of the minimum value: "<<indexC<<endl;
 
     return 0;
 }

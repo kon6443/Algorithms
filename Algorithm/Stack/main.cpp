9a961a231e2d7ac1cbd3cabd1da9e235aaefe752
Onam Kwon <kon6443@gmail.com>
2022-11-30 16:27:52 +0900

    Stack

---
+++ Algorithm/Stack/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+    stack<int> s;
+    
+    s.push(1);  // {1}
+    s.push(2);  // {1,2}
+    s.push(3);  // {1,2,3}
+    s.push(4);  // {1,2,3,4}
+
+    s.pop();    // {1,2,3}
+    s.pop();    // {1,2}
+
+    int t = s.top();  // t = 2
+
+    return 0;
+}

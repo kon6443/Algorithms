af5ed0e66955c62126ae95344fbbf2851d8531de
Onam Kwon <kon6443@gmail.com>
2022-12-08 16:58:47 +0900

    Upper & Lower bound

--- Algorithm/Sort/Lower_Upper_bound/main.cpp
+++ Algorithm/Sort/Lower_Upper_bound/main.cpp
@@ -15,23 +15,21 @@ int main() {
 
     /**
      * vector<int>::iterator lower, upper;
-     * lower_bound() returns the index value that is equal or greater than argument.
-     * 전달인자 보다 초과하는 값의 인덱스를 리턴.
+     * lower_bound() returns the iterator that is equal or greater than argument(smallest).
+     * 전달인자 보다 초과하는 값의 이터레이터 리턴.
      */
     auto lower = lower_bound(v.begin(), v.end(), 30);
     cout<<"lower_bound(30) in position at: "<<lower-v.begin()<<endl;
 
     /**
-     * upper_bound() returns the index value that is greater than argument.
-     * 전달인자 보다 이상인값의 인덱스를 리턴.
+     * upper_bound() returns the 이터레이터 that is greater than argument(smallest).
+     * 전달인자 보다 이상인값의 이터레이터 리턴.
      */
     auto upper = upper_bound(v.begin(), v.end(), 30);
     cout<<"upper_bound(30) in position at: "<<upper-v.begin()<<endl<<endl;
     
     lower = lower_bound(v.begin(), v.end(), 35);
     cout<<"lower_bound(35) in position at: "<<lower-v.begin()<<endl;
-
-    
 
     return 0;
 }

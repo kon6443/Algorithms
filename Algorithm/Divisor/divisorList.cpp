bb9818d78a8517c232a54b31bf676d67684d746b
Onam Kwon <kon6443@gmail.com>
2022-12-12 21:45:26 +0900

    Listing numbers of divisors.

--- Algorithm/Divisor/divisorList.cpp
+++ Algorithm/Divisor/divisorList.cpp
@@ -9,13 +9,14 @@ int main() {
      * Program that making a list that contains numbers of divisors.
      */
 
-    int limit = 20;
+    int limit = 10;
     vector<int> dp(limit+1);
     for(int i=1;i<=limit;i++) {
         for(int j=i;j<=limit;j+=i) {
+            cout<<j<<" ";
             dp[j]++;
-        }
-    }
+        } cout<<endl;
+    }cout<<endl<<endl;
 
     for(int i=1;i<dp.size();i++) {
         cout<<i<<": "<<dp[i]<<endl;
